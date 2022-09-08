a82a0c4d8383fe15c7472dac854511ab73cc7095
Alawaye <oreoluwaalawaye@gmail.com>
2022-09-08 12:39:13 -0700

    unix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+ * main- entry point
+ * Return: 1
+ */
+int main()
+{
+	fwrite("\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",  stderr);
+	return (1);
+}
