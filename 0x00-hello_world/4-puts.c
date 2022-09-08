54dd5e79d11bbfad91cf47b50916e241affb4820
Alawaye <oreoluwaalawaye@gmail.com>
2022-09-08 08:16:13 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 /**
+ * main - entry  point
+ *
  * using puts to print out output
+ * 
+ * returns: 0
  */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
 
