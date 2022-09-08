e6c0e3c589f44abeeb5734c3217057894bf22ead
Alawaye <oreoluwaalawaye@gmail.com>
2022-09-08 08:29:53 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - point of entry
+ *
+ * return : Alaways 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
