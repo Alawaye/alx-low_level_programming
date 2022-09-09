166d89d0e461410261c56f8441bece00d6f720b9
Alawaye <oreoluwaalawaye@gmail.com>
2022-09-09 00:54:49 -0700

    unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd>
 /**
  * main- entry point
  * Return: 1
  */
-int main()
+int main(void)
 {
-	fwrite("\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",  stderr);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 10);
 	return (1);
 }
