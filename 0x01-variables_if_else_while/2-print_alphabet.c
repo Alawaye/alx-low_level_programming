052c7cf9851de52bdc1bcff27a4c38e3ff26f956
Alawaye <oreoluwaalawaye@gmail.com>
2022-09-09 17:43:01 -0700

    done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (sucess)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+
+	return (0);
+}
+
