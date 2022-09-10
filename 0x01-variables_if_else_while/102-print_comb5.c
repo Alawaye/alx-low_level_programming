5396bfa65420e73c4762b7ba2bb204ddc5df46ba
Alawaye <oreoluwaalawaye@gmail.com>
2022-09-10 07:22:07 -0700

    done

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,10 +12,10 @@ int main(void)
 		for (b = a + 1; b <= 99; b++)
 		{
 			putchar((a / 10) + '0');
-			putchar((b % 10) + '0');
+			putchar((a % 10) + '0');
 			putchar(' ');
 			putchar((b / 10) + '0');
-			putchar((q % 10) + '0');
+			putchar((b % 10) + '0');
 			if (a == 98 && q == 99)
 				break;
 			putchar(',');
