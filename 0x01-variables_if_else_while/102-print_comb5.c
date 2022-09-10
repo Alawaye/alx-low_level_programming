c1c264e90da259ff5d728c130e7a00f04ea04427
Alawaye <oreoluwaalawaye@gmail.com>
2022-09-10 07:20:31 -0700

    done

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main- entry point
+ * Return: always 0
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; b <= 98; a++)
+	{
+		for (b = a + 1; b <= 99; b++)
+		{
+			putchar((a / 10) + '0');
+			putchar((b % 10) + '0');
+			putchar(' ');
+			putchar((b / 10) + '0');
+			putchar((q % 10) + '0');
+			if (a == 98 && q == 99)
+				break;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
