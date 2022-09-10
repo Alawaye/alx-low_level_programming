fb4c665057ae346a2af5fcb61ac6fced61df906b
Alawaye <oreoluwaalawaye@gmail.com>
2022-09-10 07:24:43 -0700

    done

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,7 +16,7 @@ int main(void)
 			putchar(' ');
 			putchar((b / 10) + '0');
 			putchar((b % 10) + '0');
-			if (a == 98 && q == 99)
+			if (a == 98 && b == 99)
 				break;
 			putchar(',');
 			putchar(' ');
