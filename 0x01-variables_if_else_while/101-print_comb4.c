356c3467ab10f5839311f926a2a40e0826f02de4
Alawaye <oreoluwaalawaye@gmail.com>
2022-09-10 06:22:26 -0700

    done

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main- Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b, c;
+
+	for (a ='0'; a < '8'; a++)
+	{
+		for (b = a + 1; b <= '8'; b++)
+		{
+			for (c = b + 1; c <= '9'; c++)
+			{
+				if (b != a)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if (a == '7' && b == '8'  && c == '9')
+						break;
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}
