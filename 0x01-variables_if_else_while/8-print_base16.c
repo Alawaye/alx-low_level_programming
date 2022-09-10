ba79b2b32897c3a8e4ec4369a724460d13df65a5
Alawaye <oreoluwaalawaye@gmail.com>
2022-09-09 19:32:54 -0700

    done

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main- entry point
+ * Return: always 0
+ */
+int main(void)
+{
+	int num;
+	char alp;
+
+	for (num = '0'; num <= '9' num++)
+		putchar(num);
+	for (alp = 'a'; alp <= 'f' alp++)
+		putchar(num);
+	putchar('\n');
+	return (0);
+}
