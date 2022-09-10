3f187a564d7b7ae71ed98aabbfc5396e8ee39582
Alawaye <oreoluwaalawaye@gmail.com>
2022-09-09 19:35:21 -0700

    done

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,7 +8,7 @@ int main(void)
 	int num;
 	char alp;
 
-	for (num = '0'; num <= '9' num++)
+	for (num = '0'; num <= '9'; num++)
 		putchar(num);
 	for (alp = 'a'; alp <= 'f'; alp++)
 		putchar(num);
