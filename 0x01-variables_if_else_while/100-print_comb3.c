1219aaae6f6099f58d34ba7a8e1dc1aa0fb156a2
Alawaye <oreoluwaalawaye@gmail.com>
2022-09-10 05:52:27 -0700

    done

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main- entry point
+ * Return: always 0
+ */
+int main(void)
+{
+	int num;
+	int nu;
+
+	for (num = '0'; num < '9'; num++)
+	{
+		for (nu = num + 1; nu <= '9'; nu++)
+		{
+			if (nu != num)
+			{
+				putchar(num);
+				putchar(nu);
+				if (num == '8' && p == '9')
+					break;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
