dce32b8342117b340b3fc2d761296579d644766e
Alawaye <oreoluwaalawaye@gmail.com>
2022-09-13 08:10:07 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main- entry point
+ * Return: always 0
+ */
+int main(void)
+{
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+		_putchar(alp);
+	_putchar('\n');
+	return (0);
+}
