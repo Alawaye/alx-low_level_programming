846ee0f31154b86a63a155a6b2fed30730192451
Alawaye <oreoluwaalawaye@gmail.com>
2022-09-13 16:52:45 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,14 +3,16 @@
  * print_last_digit - getting the last digit
  * @i: Evaluate the integer
  * Description: a function that is getting the ast digit
- * Return: 1(if n==i)
+ * Return: i (last digit)
  */
 int print_last_digit(int i)
 {
 	int i = n % 10;
 
-	if (n == i)
+	if (i < 0)
 	{
-		return (i);
+		i = i * -1;
 	}
+	_putchar (i + '0');
+	return (i);
 }
