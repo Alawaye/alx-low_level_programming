aab0a29524be99ccdef764d6b036e60970f53082
Alawaye <oreoluwaalawaye@gmail.com>
2022-09-13 16:46:26 -0700

    last_digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_last_digit - getting the last digit
+ * @i: Evaluate the integer
+ * Description: a function that is getting the ast digit
+ * Return: 1(if n==i)
+ */
+int print_last_digit(int i)
+{
+	int i = n % 10;
+
+	if (n == i)
+	{
+		return (i);
+	}
+}
