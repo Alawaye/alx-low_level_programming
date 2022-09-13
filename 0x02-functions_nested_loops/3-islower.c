8cdeef2b7d8099d3b3d527a590a3501c1f2a623e
Alawaye <oreoluwaalawaye@gmail.com>
2022-09-13 10:46:31 -0700

    done

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,20 +1,19 @@
 #include "main.h"
 /**
- * _islower- check holberton
+ * _islower - check holberton
  * @c: an input char
- * Description: lower case 
- * Return: always 0 (if it is not lowercase)
+ * Description: lowercase 
+ * Return: always 0 if it is not lowercase 1 if lowercase
  */
 int _islower(int c)
 {
 	char i;
 	int lower = 0;
-
+	
 	for (i = 'a'; i <= 'z'; i++)
 	{
 		if (i == c)
 			return (1);
 	}
-
 	return (lower);
 }
