8f8384a6da201a706d6b54408ae3415a816661ae
Alawaye <oreoluwaalawaye@gmail.com>
2022-09-13 16:27:09 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs - Getiing the abs of an interger number
+ * @r: Evaluate intger
+ * Description: Creating a function to detemine if an integer is abs or not
+ * Return: absolute value of 'r'
+ */
+int _abs(int r)
+{
+	int r;
+
+	if (r < 0)
+	{
+		return (r * -1);
+	}
+	else
+	{
+		return (r * 1);
+	}
+}
