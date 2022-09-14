68c78075d84ebddcb0afc20a594f01f912e0fadb
Alawaye <oreoluwaalawaye@gmail.com>
2022-09-14 04:36:26 -0700

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,7 +23,7 @@ void times_table(void)
 			}
 			else
 			{
-				if (y != 0)
+				if (j != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
@@ -33,7 +33,7 @@ void times_table(void)
 			}
 		}
 
-		_putchar('\n')
+		_putchar('\n');
 	}
 }
 
