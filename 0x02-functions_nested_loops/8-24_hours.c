d62d850f708475802cb561b35f37b459059075b4
Alawaye <oreoluwaalawaye@gmail.com>
2022-09-13 17:21:36 -0700

    minutes

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,13 +10,13 @@ void jack_bauer(void)
 
 	for (i = 0; i < 24; i++)
 	{
-		for (m = 0; 0 < 60; m++)
+		for (j = 0; 0 < 60; j++)
 		{
 			_putchar((i / 10) + '0');
-			_putchar((H % 10) + '0');
+			_putchar((i % 10) + '0');
 			_putchar(':');
-			_putchar((M / 10) + '0');
-			_putchar((M % 10) + '0');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
 			_putchar('\n');
 		}
 	}
