d678ff54edf34ff35e3f7a44bf253215862930ea
Alawaye <oreoluwaalawaye@gmail.com>
2022-09-15 11:07:47 -0700

    digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#incude "main.h"
+/**
+ * _isdigit - using ascii get digits from 0-9
+ * @c: evaluating integer
+ * Return:1(if digits from 0-9) 0(if digits is not from 0-9)
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
