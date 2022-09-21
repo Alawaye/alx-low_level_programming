6d4967bb137a750eac0fa4cb26329633826f8663
Alawaye <oreoluwaalawaye@gmail.com>
2022-09-21 15:16:37 -0700

    done

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,12 +11,12 @@ char *_strcat(char *dest, char *src);
 
 	while (dest[i] != '\0')
 	{
-		++i;
+		i++;
 	}
 
-	for (j = 0; src[j] != '\0'; ++j, ++i)
+	for (j = 0; src[j] != '\0'; j++)
 	{
-		dest[i] = src[j];
+		dest[i++] = src[j];
 	}
 	dest[i] = '\0';
 	return (dest);
