a0f4fa50439e13d98c24cfa34866c2f2e1b08580
Alawaye <oreoluwaalawaye@gmail.com>
2022-09-21 14:30:53 -0700

    done

--- 0-strcat.c
+++ 0-strcat.c
@@ -1,14 +1,14 @@
+#include "main.h"
 #include <stdio.h>
 #include <string.h>
 /**
- * *_strcat - 
- * @dest:
- * @src:
- * return:
+ * _strcat - concatenating string
+ * @dest: destination string
+ * @src: source string
+ * Return: dest
  */
 char *_strcat(char *dest, char *src)
 {
 	strcat(dest, src);
 	return (dest);
 }
-
