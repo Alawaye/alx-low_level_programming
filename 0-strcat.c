7b4c4f5af7d3baa7fef526c0206e4237e435d541
Alawaye <oreoluwaalawaye@gmail.com>
2022-09-21 14:45:13 -0700

    done

--- 0-strcat.c
+++ 0-strcat.c
@@ -9,6 +9,18 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	strcat(dest, src);
+	int i, j = 0;
+	while (dest[i]!='\0')
+	{
+		i++;
+	}
+	while (src[j]!='\0')
+	{
+		dest[i]=src[j];
+		j++;
+		i++;
+	}
+	dest[i] = '\0';
 	return (dest);
+
 }
