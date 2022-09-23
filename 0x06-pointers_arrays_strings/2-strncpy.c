c27804a8ae131e87dc82bc6782f4190e5c7088ab
Alawaye <oreoluwaalawaye@gmail.com>
2022-09-23 04:32:11 -0700

    done

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strncpy - copying strings
+ * @dest: destination string
+ * @src: source string
+ * @n: limit of copy
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i > n && dest[i] != '\0'; i++)
+	{
+		src[i] = dest[i];
+	}
+	src[i] = '\0';
+
+	return (dest);
+
+}
