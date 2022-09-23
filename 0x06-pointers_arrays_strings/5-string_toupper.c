ce9df0344d6e3d69d77a24622ff58dbb6208c5c0
Alawaye <oreoluwaalawaye@gmail.com>
2022-09-23 06:10:04 -0700

    done

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - converting lowercase to uppercase
+ * Return: char
+ * @c:char to be evaluated
+ */
+char *string_toupper(char *c)
+{
+	int i;
+
+	while (c[i] != '\0')
+	{
+		if (c[i] >= 'a' && 'c[i] <= 'z')
+		{
+			c[i] = c[i] - 32;
+		}
+		i++;
+	}
+	return (c);
+}
