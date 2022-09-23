8bee81875b6264970e0f2840d8b2fab1bd976c3f
Alawaye <oreoluwaalawaye@gmail.com>
2022-09-23 05:23:40 -0700

    done

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,16 +10,15 @@ int _strcmp(char *s1, char *s2)
 	int i;
 	int flag = 0;
 
-	for (i = 0;  *s1[i] != '\0' || *s2[i] != '\0'; i++)
+	for (i = 0;  s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
-		if (*s1[i] != *s2[i])
+		if (s1[i] != s2[i])
 		{
-			flag = 1;
-			break
+			return (s1[i] - s2[i]);
 		}
 	}
-	if (flag == 1)
-		return (1);
-	else
+	if (flag == 0)
 		return (0);
+	else
+		return (-1);
 }
