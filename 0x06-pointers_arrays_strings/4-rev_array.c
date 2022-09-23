a4d450e433280f9d1c664df2a96d85f5392eee7b
Alawaye <oreoluwaalawaye@gmail.com>
2022-09-23 05:55:56 -0700

    done

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * reverse_array - reversing an array of integers
+ * @a: integrs to be evaluated 
+ * @n: number of element of array
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int i, temp;
+
+	for (i = 0; i < n/2; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = temp;
+	}
+}
