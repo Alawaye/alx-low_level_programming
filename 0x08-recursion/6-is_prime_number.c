eae783c4f2a6a3bef18b58919f123aa00f3c0c1a
Alawaye <oreoluwaalawaye@gmail.com>
2022-09-28 13:49:37 -0700

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,21 +1,43 @@
 #include "main.h"
+#include <stdio.h>
+
+int checkPrime(int i, int num);
+
+
+
 /**
  * is_prime_number - checking for prime numbers
  * @n: evaluating numbers
  * Return: always 0
  */
+
 int is_prime_number(int n)
 {
-	int i = 2;
+	return (CheckPrime(2, num));
+}
 
-	if (n == i)
-	{
-		return (1);
-	}
-	else if (n % i == 0)
+/**
+ * CheckPrime - checking for prime numbers
+ * @i : number to evaluate
+ * @n: iterate number
+ * Return: 0(not prime) 1(prime)
+ */
+
+
+int CheckPrime(int i, int num)
+{
+	if (num == i)
 	{
 		return (0);
 	}
-	i++;
-	return (is_prime_number(n));
+
+	else if (num % i == 0)
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (CheckPrime(i + 1, num));
+	}
 }
