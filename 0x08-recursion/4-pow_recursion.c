5744a0e21ac688edc6bcc00cf29ebcc7050facc9
Alawaye <oreoluwaalawaye@gmail.com>
2022-09-28 04:40:41 -0700

    done

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _pow_recursion - getting power of numbers
+ * @x: base
+ * @y: power
+ * Return: -1(if lower than 0)
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	
+	else if (y == 0)
+	{
+		return (1);
+	}
+	
+	else
+	{
+		return  (a * _pow_recursion(a, b - 1));
+	}
+}
