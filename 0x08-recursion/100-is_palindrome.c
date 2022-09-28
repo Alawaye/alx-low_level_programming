5649d1c5ec766eee707d655677866fbcbfe758e9
Alawaye <oreoluwaalawaye@gmail.com>
2022-09-28 11:28:15 -0700

    task 7

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,28 @@
+#include "maain.h"
+/**
+ * is_palindrome - checking if string is palindrome or not
+ * @s: evauating char
+ * Return: always 0(sucess)
+ */
+int is_palindrome(char *s)
+{
+	if (*s ==  _print_rev_recursion(s))
+	{
+		return (1);
+	}
+
+	return (0);
+}
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	else
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
