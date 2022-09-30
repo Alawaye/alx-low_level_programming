124240f814dce068545195136e89ce8a00b36125
Alawaye <oreoluwaalawaye@gmail.com>
2022-09-30 06:46:56 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - printing number of argument
+ * @argc: argument count
+ * @argv: argument vector/array
+ * Return: 0(sucess)
+ */
+int main (int argc char *argv[])
+{
+	printf("%d\n", argc - 1);
+}
