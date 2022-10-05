fadb99797bacc1a96da4b3e016604f5ddd320da9
Alawaye <oreoluwaalawaye@gmail.com>
2022-10-05 05:55:13 -0700

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - Duplicates a string
+ *@str: String
+ *Return: NULL or str_2
+ */
+char *_strdup(char *str)
+{
+
+	int i, j;
+	char *str_2;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	str_2 = malloc((i + 1) * sizeof(char));
+	if (str_2 == NULL)
+		return (NULL);
+
+	for (j = 0; j < (i + 1); j++)
+		str_2[j] = str[j];
+
+	return (str_2);
+}
