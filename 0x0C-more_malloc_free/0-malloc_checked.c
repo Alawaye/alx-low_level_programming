8fe59cd9ce9c0c2357d543f8920b98a8f96f485a
Alawaye <oreoluwaalawaye@gmail.com>
2022-10-06 04:52:05 -0700

    task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocating memory using malloc
+ * @b : unisigned integer
+ * Return: pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == Null)
+		exit(98);
+
+	return (ptr);
+}
