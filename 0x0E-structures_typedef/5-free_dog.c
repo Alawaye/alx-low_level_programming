823de59e8fdadc4f908a9f91693f2e81be4ff174
Alawaye <oreoluwaalawaye@gmail.com>
2022-10-10 14:12:26 -0700

    task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ *free_dog- Frees memory allocated to struct dog
+ *@d: Pointer to struct
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != Null)
+	{
+		free((d->name);
+		free(d->age);
+		free(d->owner);
+		free(d);
+	}
+}
+
+
