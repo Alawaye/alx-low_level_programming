c873b38f552dadf9c7a3afe7509c24bac151fe28
Alawaye <oreoluwaalawaye@gmail.com>
2022-11-04 16:41:56 -0700

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,9 +1,11 @@
 #include "main.h"
-#include <stdio.>
+#include <stdio.h>
 #include <stdlib.h>
+
 
 char *create_buffer(char *file);
 void close_file(int fd);
+
 
 /**
  *create_buffer - Allocates 1024 bytes for a buffer.
@@ -28,7 +30,6 @@ char *create_buffer(char *file)
 }
 
 
-
 /**
  *close_file - Closes file descriptors.
  *@fd: The file descriptor to be closed.
@@ -42,11 +43,10 @@ void close_file(int fd)
 
 	if (c == -1)
 	{
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-			exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
 	}
 }
-
 
 /**
  *main - Copies the contents of a file to another file.
@@ -56,7 +56,7 @@ void close_file(int fd)
  *Description: If the argument count is incorrect - exit code 97.
  *              If file_from does not exist or cannot be read - exit code 98.
  *              If file_to cannot be created or written to - exit code 99.
- *              If file_to or file_from cannot be closed - exit code 100.
+ *		If file_to or file_from cannot be closed - exit code 100.
  */
 
 int main(int argc, char *argv[])
